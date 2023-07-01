9e3524db360de4a89e2dbc2e278ddb44a775a8ae
VIJAY-GADRE <vjgadre21@gmail.com>
2023-07-01 19:37:08 +0530

    Updated the variable deps

--- preprocessor.py
+++ preprocessor.py
@@ -10,15 +10,15 @@ def preprocess(data):
     messages = re.split(regex_pattern, data)[1:]
     dates = re.findall(regex_pattern, data)
 
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    dataframe = pd.DataFrame({'user_message': messages, 'message_date': dates})
     try:
-        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+        dataframe['message_date'] = pd.to_datetime(dataframe['message_date'], format='%d/%m/%y, %H:%M - ')
     except:
-        df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %H:%M - ')
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+        dataframe['message_date'] = pd.to_datetime(dataframe['message_date'], format='%m/%d/%y, %H:%M - ')
+    dataframe.rename(columns={'message_date': 'date'}, inplace=True)
 
     users, messages = [], []
-    for message in df['user_message']:
+    for message in dataframe['user_message']:
         entry = re.split('([\w\W]+?):\s', message)
         if entry[1:]:
             users.append(entry[1])                       # Username
@@ -27,18 +27,18 @@ def preprocess(data):
             users.append('group_notification')
             messages.append(entry[0])
 
-    df['user'] = users
-    df['message'] = messages
+    dataframe['user'] = users
+    dataframe['message'] = messages
 
-    df.drop(columns=['user_message'], inplace=True)
-    df['only_date'] = df['date'].dt.date
-    df['year'] = df['date'].dt.year
-    df['month_num'] = df['date'].dt.month
-    df['month'] = df['date'].dt.month_name()
-    df['day'] = df['date'].dt.day
-    df['day_name'] = df['date'].dt.day_name()
-    df['hour'] = df['date'].dt.hour
-    df['minute'] = df['date'].dt.minute
-    df = df[df['user'] != 'group_notification']
+    dataframe.drop(columns=['user_message'], inplace=True)
+    dataframe['only_date'] = dataframe['date'].dt.date
+    dataframe['year'] = dataframe['date'].dt.year
+    dataframe['month_num'] = dataframe['date'].dt.month
+    dataframe['month'] = dataframe['date'].dt.month_name()
+    dataframe['day'] = dataframe['date'].dt.day
+    dataframe['day_name'] = dataframe['date'].dt.day_name()
+    dataframe['hour'] = dataframe['date'].dt.hour
+    dataframe['minute'] = dataframe['date'].dt.minute
+    dataframe = dataframe[dataframe['user'] != 'group_notification']
 
-    return df
+    return dataframe
